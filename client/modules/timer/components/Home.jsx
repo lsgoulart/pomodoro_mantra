fb8d49a74cf430f85f96811cea12cd19bf202fe0
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2016-04-01 10:42:52 -0300

    adding title

--- client/modules/timer/components/Home.jsx
+++ client/modules/timer/components/Home.jsx
@@ -8,6 +8,7 @@ class Home extends Component {
         return(
             <div className="timer-app">
                 <Helmet title="Timer" />
+                <h1>Pomodoro Timer</h1>
                 <button onClick={this.newTimer.bind(this, 'pomodoro')}>pomodoro</button>
                 <button onClick={this.newTimer.bind(this, 'shortbreak')}>short break</button>
                 <button onClick={this.newTimer.bind(this, 'longbreak')}>long break</button>
